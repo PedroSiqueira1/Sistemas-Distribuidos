084882235900c5ac23d9982bd250ea6bb298acdf
PedroSiqueira1 <pedro.siqueira@poli.ufrj.br>
2023-04-24 15:24:45 -0300

    Add waits to signal_handler

--- TP1/Signals/signal_handler.cpp
+++ TP1/Signals/signal_handler.cpp
@@ -23,17 +23,30 @@ void signalHandler( int signum ) {
    }
 }
 
-int main () {
+int main (int argc, char *argv[]) {
+
+   // Shows the PID of the process
+   std::cout << "PID: " << getpid() << std::endl;
+   
    // register signal SIGINT and signal handler  
    signal(1, signalHandler);
    signal(2, signalHandler);
    signal(3, signalHandler);
    
-     
+   // Busy wait for signal
+   if (atoi(argv[1]) == 0) {
+      while(1) {
+         std::cout << "Going to sleep...." << std::endl;
+         sleep(1);
+      }
+   }
 
-   while(1){
-      std::cout << "Going to sleep...." << std::endl;
-      sleep(1);
+   // Blocking wait for signal
+   if (atoi(argv[1]) == 1) {
+      while(1) {
+         pause();
+      }
+      
    }
    
    return 0;
